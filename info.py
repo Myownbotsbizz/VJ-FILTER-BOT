f0c76416717f27d752aeac042264332bf7ece84f
MoviesbizzYT <89892012+MoviesbizzYT@users.noreply.github.com>
2024-07-30 09:22:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '6125726'))
+API_HASH = environ.get('API_HASH', 'c2db15328ed3f991683ae71a82fca20e')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
